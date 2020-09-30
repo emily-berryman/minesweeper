942eba07b273f30c0d3fff29024cb0da9e100829
Emily Berryman <emilyrberryman@gmail.com>
2020-09-30 13:38:41 +1300

    start checkForWin function

--- minesweeper.js
+++ minesweeper.js
@@ -68,51 +68,80 @@ var board = {
 
 
 function startGame () {
-  for(var i = 0; i < board['cells'].length; i++) 
-   var cell = board['cells'][i]
-   {
-   var mineCount = countSurroundingMines(cell) 
-   mineCount = board['cells'].surroundingMines
-   //   if(board['cells'][i].isMine == true){
-   //      console.log(board['cells'][i].row + "," + board['cells'][i].col)
+  for(var i = 0; i < board['cells'].length; i++){
+      var currentCell = board.cells[i]
+      currentCell.surroundingMines = countSurroundingMines(currentCell)
+ }
    lib.initBoard()     
+   document.addEventListener("click", checkForWin)
+   document.addEventListener("contextmenu", checkForWin)
    }
-  }
-
-
+  
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+   for(var i = 0; i < board.cells.length; i++){
+      var currentCell = board.cells[i]
+      if (currentCell.isMine == true && currentCell.isMarked == false) {
+         return;
+      } else if (currentCell.hidden == true) {
+         return;
+      }
+      }
+       return lib.displayMessage('You win!')
+   }
+
+
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+ 
+
+
+   // If every mine is marked, but there are still cells with the hidden property set to true, the player hasn't won yet and you can return out of the function.
+   // // Create a loop to run through the properties contained in each of the cells
+   // // each cell should have .isMine & .isMarked should be true for 
+
+   // or each cell, check to see if both .isMine and .isMarked are true. If any mine still exists that isn't marked, the player hasn't won yet and you can return to exit out of the function.
+
+// // Define this function to count the number of mines around the cell
+// // (there could be as many as 8). You don't have to get the surrounding
+// // cells yourself! Just use `lib.getSurroundingCells`: 
+// //
+// //   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+// //
+// // It will return cell objects in an array. You should loop through 
+// // them, counting the number of times `cell.isMine` is true.
+// function countSurroundingMines (cell) {
+// //    var row = board.cells.row
+// //    var col = board.cells.col
+// //    var surrounding = lib.getSurroundingCells()
+// //    var count = 0
+// //    for (var i = 0; i < surrounding.length; i++ ){
+// //    return surrounding[i]
+// //    }
+// }
+
+// What we want: 
+
+//write a function: countSurroundingMines 
+
+// which takes in a cell and gives us the number of mines 
+
+// 1. Get the row & col from the cell
+// 2. Use getSurroundingCells to get the array of adjacent cells
+// 3. loop over and count the number of mines
+
+function countSurroundingMines(cellAskKelly) {
+   var adjacentCells = getSurroundingCells(cellAskKelly.row, cellAskKelly.col)
+   var count = 0
+   for (var i = 0; i < adjacentCells.length; i++){
+      if (adjacentCells[i].isMine == true){
+         count++
+      }
+   }
+   return count
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
-//    var row = board.cells.row
-//    var col = board.cells.col
-//    var surrounding = lib.getSurroundingCells()
-//    var count = 0
-//    for (var i = 0; i < surrounding.length; i++ ){
-//    return surrounding[i]
-//    }
-}
-
-console.log(board)
-
-// a new function takes a cell as an argument, creates a new variable that is equal to the surrounding cells (using  lib.getSurroundingCells that they provided and calling it on the row and col of the cell) and another new variable (that I named count) that is equal to 0.
-// The function cycles through so long as i<surrounding.length and IF the i'th value of surrounding isMine then count increases by one.
-// Finally the count is returned.
-
-countSurroundingMines()
